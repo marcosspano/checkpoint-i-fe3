991b40fcc07fdf4d21a9c451c59318c6ed7118e5
marcosspano <e.marcosspano@gmail.com>
2021-11-17 02:44:19 -0300

    Correção Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,8 +10,8 @@ const Footer = () => {
             </div>
 
             <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-                <li className="ms-3"><a className="text-muted" href="https://github.com/marcosspano" target="_blank"><i className="bi-github" role="img" aria-label="GitHub" style={{fontSize: "28px"}} /></a></li>
-                <li className="ms-3"><a className="text-muted" href="https://www.linkedin.com/in/marcosspano/" target="_blank"><i className="bi-linkedin" role="img" aria-label="Linkedin" style={{fontSize: "28px"}} /></a></li>
+                <li className="ms-3"><a className="text-muted" href="https://github.com/marcosspano" target="blank"><i className="bi-github" role="img" aria-label="GitHub" style={{fontSize: "28px"}} /></a></li>
+                <li className="ms-3"><a className="text-muted" href="https://www.linkedin.com/in/marcosspano/" target="blank"><i className="bi-linkedin" role="img" aria-label="Linkedin" style={{fontSize: "28px"}} /></a></li>
                 <li className="ms-3"><a className="text-muted" href="mailto:e.marcosspano@gmail.com"><i className="bi-envelope-fill" role="img" aria-label="Mail" style={{fontSize: "28px"}} /></a></li>
             </ul>
         </footer>
